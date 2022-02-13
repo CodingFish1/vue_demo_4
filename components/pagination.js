e09132fa1d1512d7f08b6b414108a71f6ff3d903
CodingFisher <cppmaster@outlook.com>
2022-02-13 19:51:30 +1030

    pagination finished

--- components/pagination.js
+++ components/pagination.js
@@ -4,7 +4,7 @@ export default{
             current_page:"",
         };
     },
-    props:[`paginationjs`],
+    props:[`paginationjs`,`currpagejs`],
     methods:{
       pageTrans(page){
         this.current_page=page;
@@ -16,14 +16,14 @@ export default{
     template:`<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item " :class="paginationjs.has_pre ? 'enabled':'disabled'">
-        <a class="page-link" href="" aria-label="Previous">
+        <a class="page-link" href="" aria-label="Previous" @click.prevent="pageTrans(page=currpagejs-1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <li class="page-item" v-for="page in paginationjs.total_pages" :key="page" :class="{active:page===paginationjs.current_page}">
       <a class="page-link" href="" @click.prevent="pageTrans(page)">{{page}}</a></li>
       <li class="page-item" v-bind:class="paginationjs.has_next ? 'enabled':'disabled'">
-        <a class="page-link" href="" aria-label="Next">
+        <a class="page-link" href="" aria-label="Next" @click.prevent="pageTrans(page=currpagejs+1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
