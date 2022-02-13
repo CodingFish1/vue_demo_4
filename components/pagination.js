33b770a6f11732c32a8165c2de88837783def492
CodingFisher <cppmaster@outlook.com>
2022-02-13 19:54:27 +1030

    pagination name fixed

--- components/pagination.js
+++ components/pagination.js
@@ -16,14 +16,14 @@ export default{
     template:`<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item " :class="paginationjs.has_pre ? 'enabled':'disabled'">
-        <a class="page-link" href="" aria-label="Previous" @click.prevent="pageTrans(page=currpagejs-1)">
+        <a class="page-link" href="" aria-label="Previous" @click.prevent="pageTrans(pagepre=currpagejs-1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <li class="page-item" v-for="page in paginationjs.total_pages" :key="page" :class="{active:page===paginationjs.current_page}">
       <a class="page-link" href="" @click.prevent="pageTrans(page)">{{page}}</a></li>
       <li class="page-item" v-bind:class="paginationjs.has_next ? 'enabled':'disabled'">
-        <a class="page-link" href="" aria-label="Next" @click.prevent="pageTrans(page=currpagejs+1)">
+        <a class="page-link" href="" aria-label="Next" @click.prevent="pageTrans(pagenxt=currpagejs+1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
